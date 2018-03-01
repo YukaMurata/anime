e7e57bedfbf56d1db2575de5b7a7be66c818a2ea
murata-yuka <murata-yuka@9105-109520.local>
2018-03-01 19:07:00 +0900

    first commit

---
+++ resources/js/index.js
@@ -0,0 +1,8 @@
+var Animation = require('./modules/animation');
+var ClickAnimation = require('./modules/clickAnimation');
+
+var animation = new Animation();
+var clickAnimation = new ClickAnimation();
+
+
+
